2e65450b58247bbe3ad06169a7897fd6dfe9fc69
Vander <vandermaziero@gmail.com>
2019-04-26 08:08:56 -0300

    Initial Commit

---
+++ iot-ios-sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iot-ios-sandbox
+//
+//  Created by ITLABS WEG on 26/04/19.
+//  Copyright © 2019 WEG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
